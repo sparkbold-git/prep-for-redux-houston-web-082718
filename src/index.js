83bdb68ef78cec4b34edc4016c0c25efd33c6f50
sparkbold-git <sparkboldstudio@gmail.com>
2018-11-15 10:35:02 -0600

    done

--- src/index.js
+++ src/index.js
@@ -1,15 +1,21 @@
-import React, { Component } from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import logo from "./logo.svg";
+import "./App.css";
 
 class App extends Component {
+  state = { count: 0 };
+
+  updateCount = count => {
+    this.setState({ count });
+  };
+
   render() {
     return (
       <div className="App">
-        <Header />
-        <Counter />
+        <Header count={this.state.count} />
+        <Counter updateCount={this.updateCount} />
       </div>
     );
   }
@@ -17,10 +23,13 @@ class App extends Component {
 
 class Header extends Component {
   render() {
+    const remainder = this.props.count % 5;
+    const upToNext = 5 - remainder;
     return (
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h1 className="App-title">Welcome to React</h1>
+        <h1 className="App-title">Welcome to Flatiron</h1>
+        <h3>The current count is less than {this.props.count + upToNext}</h3>;
       </header>
     );
   }
@@ -31,16 +40,18 @@ class Counter extends Component {
 
   increment = () => {
     this.setState(prevState => ({ count: prevState.count + 1 }));
+    this.props.updateCount(this.state.count);
   };
 
   decrement = () => {
     this.setState(prevState => ({ count: prevState.count - 1 }));
+    this.props.updateCount(this.state.count);
   };
 
   renderDescription = () => {
     const remainder = this.state.count % 5;
     const upToNext = 5 - remainder;
-    return `The current count is less than ${this.state.count + upToNext}`;
+    // return `The current count is less than ${this.state.count + upToNext}`;
   };
 
   render() {
@@ -49,10 +60,10 @@ class Counter extends Component {
         <h1>{this.state.count}</h1>
         <button onClick={this.decrement}> - </button>
         <button onClick={this.increment}> + </button>
-        <h3>{this.renderDescription()}</h3>
+        {/* <h3>{this.renderDescription()}</h3> */}
       </div>
     );
   }
 }
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById("root"));
